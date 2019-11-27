2dd2d0ccdf53a509038ef59f8fd665e5f3a182e9
Adrien RICHARD <richardadrien0@gmail.com>
2019-11-27 14:05:29 +0100

    exo 4 corrected

--- Exo4.py
+++ Exo4.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import random
 rand = random.randint(1, 100)
+print(str(rand))
 userguess = 999
+tries = 0
 while int(userguess) != rand:
     userguess = input("Votre nombre : ")
+    tries += 1
     if int(userguess) > rand:
         print("C'est plus bas !")
     if int(userguess) < rand:
         print("C'est plus haut !")
-print("Victoire ! le chiffre etais " + userguess + " !")
+print("Victoire en " + str(tries) + " coups! Le chiffre etais " + userguess + " !")
