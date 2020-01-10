aec8e4b7f738cad6cf2e0e1a9c33e4a39f68ba0b
Adrien RICHARD <richardadrien0@gmail.com>
2020-01-10 14:12:24 +0100

    scapper v2

--- webscrapper/scrapper.py
+++ webscrapper/scrapper.py
@@ -1,17 +1,32 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import sys
 
-listt = ["hello","bye","good+day"]
+scoretab = []
+
+def score(listresult):
+    total = 0
+    listresult.sort(reverse = True)
+    for t in listresult:
+        total += t
+    percent = listresult[0] / total
+    print(percent*100)
 
 def search(searchStr):
     req = urllib.request.Request(
-    url="https://www.google.com/search?q="+searchStr,
-    headers={'User-Agent': ' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'})
+        url="https://www.google.com/search?hl=en&q="+searchStr,
+        headers={'User-Agent': ' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'})
     handler = urllib.request.urlopen(req)
     with handler as response:
         soup = BeautifulSoup(response, 'html.parser')
         for resultStats in soup.find_all("div", {"id": "resultStats"}):
-            print(resultStats)
-            
-for searchString in listt:
-    search(searchString)
+            string = resultStats.contents[0]  # gets the content
+            stringsplit = string.split()  # splits the string
+            # transformes the string to an int
+            stringtoint = int(stringsplit[1].replace(',', ''))
+            scoretab.append(stringtoint)
+            print(searchStr," ",stringtoint )
+
+for i in range(1, len(sys.argv)):
+    search(sys.argv[i])
+score(scoretab)
